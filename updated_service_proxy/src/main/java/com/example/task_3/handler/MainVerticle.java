a0fc52b9423904c8e8f2ba2bcad2d9432d1ee077
Kranthi-Guribilli <kranthi.guribilli@datakaveri.org>
2023-09-21 12:26:47 +0530

    Add files via upload

---
+++ updated_service_proxy/src/main/java/com/example/task_3/handler/MainVerticle.java
@@ -0,0 +1,32 @@
+package com.example.task_3.handler;
+import com.example.task_3.database.MongoDBVerticle;
+import com.example.task_3.restapi.RestAPIVerticle;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.DeploymentOptions;
+
+public class MainVerticle extends AbstractVerticle{
+  @Override
+  public void start() {
+    Vertx vertx = Vertx.vertx();
+
+    // Deploy DeviceRepositoryVerticle
+    vertx.deployVerticle(MongoDBVerticle.class.getName(), new DeploymentOptions().setInstances(1), res1 -> {
+      if (res1.succeeded()) {
+        System.out.println("MongoDBVerticle deployed successfully");
+      } else {
+        System.out.println("MongoDBVerticle deployment failed");
+      }
+
+      // Deploy RestAPIVerticle
+      vertx.deployVerticle(RestAPIVerticle.class.getName(), new DeploymentOptions().setInstances(1), res2 -> {
+        if (res2.succeeded()) {
+          System.out.println("RestAPIVerticle deployed");
+        } else {
+          System.out.println("RestAPIVerticle deployment failed");
+        }
+      });
+    });
+  }
+}
+
